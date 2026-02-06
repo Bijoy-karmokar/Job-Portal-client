94388b4128059485488f44595d196444bcdd6f62
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-02-06 15:42:49 +0600

    Login

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import Lottie from "lottie-react";
+import React from "react";
+import LogInLottie from '../assets/Login.json'
 
 const LogIn = () => {
-    return (
-        <div>
-            
+    const handleLogIn=(e)=>{
+       e.preventDefault();
+       const form = e.target; 
+       const formData = new FormData(form);
+       const LogInData = Object.fromEntries(formData.entries());
+       console.log(LogInData);
+       
+    }
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <Lottie animationData={LogInLottie} style={{width:500,height:500}} loop></Lottie>
         </div>
-    );
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+           <h1 className="text-5xl font-bold">Login now!</h1>
+            <form onSubmit={handleLogIn} className="fieldset">
+              <label className="label">Email</label>
+              <input type="email" name="email" className="input" placeholder="Email" />
+              <label className="label">Password</label>
+              <input type="password" name="password" className="input" placeholder="Password" />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,27 +4,33 @@ import registerLottie from "../assets/Register.json";
 
 const Register = () => {
   const handleRegister =(e)=>{
-     e.preventDefault();
-     const form = e.target;
-     console.log(form);
+    const form = e.target;
+    e.preventDefault();
+    const formData = new FormData(form);
+    const RegisterData = Object.fromEntries(formData.entries());
+    console.log(RegisterData);
+    
   }
 
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          <Lottie  animationData={registerLottie} loop />
+          <Lottie style={{width:700,height:700}} animationData={registerLottie} loop />
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
             <h1 className="text-5xl font-bold">Register now!</h1>
             <form onSubmit={handleRegister} className="fieldset">
+              <label className="label">Name</label>
+              <input type="text" name='name' className="input" placeholder="Enter your name" />
+              <label className="label">Photo</label>
+              <input type="text" name='photo' className="input" placeholder="Enter your photoURL" />
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input type="email" name='email' className="input" placeholder="Email" />
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div><a className="link link-hover">Forgot password?</a></div>
-              <button className="btn btn-neutral mt-4">Login</button>
+              <input type="password" name='password' className="input" placeholder="Password" />
+              <button className="btn btn-neutral mt-4">Register</button>
             </form>
           </div>
         </div>
